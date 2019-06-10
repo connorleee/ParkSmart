f1fd5eee686ef18507233bedf86e30c01b2e3001
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-06-09 21:45:48 -0700

    More spaces deleted after "function"

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Parking.findOne({ where: { id: req.params.id } }).then(function (
+    db.Parking.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.render("example", {
