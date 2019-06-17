71990ead09fac6bb10c4fc22bf574572668a6b5b
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-06-16 17:16:57 -0700

    map now displays. need to fix css to have map take up the remainder of the viewport. need to fix link between homepage and map

--- public/js/map.js
+++ public/js/map.js
@@ -1,50 +1,50 @@
-$(document).ready(function() {
-  $.ajax({
-    method: "GET",
-    url: "/api/Parkings/"
-  }).then(function(response) {
-    console.log(response);
-    function initMap() {
-      var center = {
-        lat: 34.052235,
-        lng: -118.243683
-      };
-      var locations = [];
-      for (let i = 0; i < response.length; i++) {
-        locations.push([response[i][1], response[i][2]]);
-      }
-      console.log(locations);
-      var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 9,
-        center: center
-      });
-      var infowindow = new google.maps.InfoWindow({});
-      var marker, count;
-      for (count = 0; count < locations.length; count++) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(
-            locations[count][1],
-            locations[count][2]
-          ),
-          map: map,
-          title: locations[count][0]
-        });
-        google.maps.event.addListener(
-          marker,
-          "click",
-          (function(marker, count) {
-            return function() {
-              infowindow.setContent(locations[count][0]);
-              infowindow.open(map, marker);
-            };
-          })(marker, count)
-        );
-      }
-    }
+// $(document).ready(function() {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/Parkings/"
+//   }).then(function(response) {
+//     console.log(response);
 
-    initMap();
+function initMap() {
+  console.log("init map running");
+  var center = {
+    lat: 47.610112,
+    lng: -122.324838
+  };
+  var locations = [];
+  // for (let i = 0; i < response.length; i++) {
+  //   locations.push([response[i][1], response[i][2]]);
+  // }
+  console.log(locations);
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: center
   });
-});
+  var infowindow = new google.maps.InfoWindow({});
+  var marker, count;
+  for (count = 0; count < locations.length; count++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(
+        locations[count][1],
+        locations[count][2]
+      ),
+      map: map,
+      title: locations[count][0]
+    });
+    google.maps.event.addListener(
+      marker,
+      "click",
+      (function(marker, count) {
+        return function() {
+          infowindow.setContent(locations[count][0]);
+          infowindow.open(map, marker);
+        };
+      })(marker, count)
+    );
+  }
+}
+//   });
+// });
 
 function preloadLocationValue() {
   //load the city and state name from local storage to location box
