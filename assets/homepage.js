fc895598a809c5f5320fc42e686b7c673e651ee0
KevinL <mastahkevin@gmail.com>
2019-06-16 21:40:50 -0700

    Deleted 8 files and merged/added others.

--- assets/homepage.js
+++
@@ -1,8 +0,0 @@
-function searchCity() {
-  console.log("I am here");
-  let searchInput = document.getElementById("user-choice").value;
-  localStorage.searchInput = searchInput;
-  window.location.href = "index.html";
-}
-
-searchCity();
--- assets/index.js
+++
@@ -1,7 +0,0 @@
-function preloadLocationValue() {
-  let locationField = document.getElementById("location-input");
-  let preloadValue = localStorage.searchInput;
-  locationField.value = preloadValue;
-}
-
-window.onload = preloadLocationValue;
